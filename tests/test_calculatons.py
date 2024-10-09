4062c410130a9f2722137c7506d273f869035085
root <root@Asussybaka>
2024-10-09 13:02:39 -0400

    Updated file with final changes for part2

--- tests/test_calculatons.py
+++ tests/test_calculatons.py
@@ -1,11 +1,11 @@
-from calculator import add, subtract,divide, multiply
+from calculator.operations import add, multiply, subtract, divide
 
 def test_addition():
     assert add(2,2) == 4
 
 def test_subtraction():
     assert subtract(2,2) == 0
-    
+
 def test_multiplication():
     assert multiply(2,2) == 4
 
