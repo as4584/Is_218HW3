3431fce11b24fe98f500affec17a2e7739cbe5b5
root <root@Asussybaka>
2024-10-09 13:28:40 -0400

    Updated file with final changes for part2

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -1,8 +1,29 @@
-class Calculation:
-    def __init__(self, a, b, operation):
-        self.a = a
-        self.b = b
-        self.operation = operation  
+from decimal import Decimal
+from typing import Callable, List
 
-    def get_result(self):
-        return self.operation(self.a, self.b)
+from calculator.calculation import Calculation
+
+class Calculations:
+    history: List[Calculation] = []
+
+    @classmethod
+    def add_calculation(cls, calculation: Calculation):
+        cls.history.append(calculation)
+
+    @classmethod
+    def get_history(cls) -> List[Calculation]:
+        return cls.history
+
+    @classmethod
+    def clear_history(cls):
+        cls.history.clear()
+
+    @classmethod
+    def get_latest(cls) -> Calculation:
+        if cls.history:
+            return cls.history[-1]
+        return None
+
+    @classmethod
+    def find_by_operation(cls, operation_name: str) -> List[Calculation]:
+        return [calc for calc in cls.history if calc.operation.__name__ == operation_name]
